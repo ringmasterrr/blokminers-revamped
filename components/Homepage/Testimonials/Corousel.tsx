c5e7bc982a60c4f1230f47690097cd45d3dba7f2
YashT1005 <yashthakur10052@gmail.com>
2024-06-25 05:24:10 +0530

    testimonials industries explore

--- components/Homepage/Testimonials/Corousel.tsx
+++ components/Homepage/Testimonials/Corousel.tsx
@@ -8,115 +8,119 @@ import { useRef, useState } from 'react'
 import { Navigation, Pagination } from 'swiper/modules'
 import { GoStarFill } from 'react-icons/go'
 import Image from 'next/image'
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 
 SwiperCore.use([Navigation])
 
 const Corousel = () => {
-    const [slideData] = useState([
-        {
-            name: 'First slide',
-            message:
-                'The dedication, integrity, and commitment coupled with the extensive knowledge in Web3.O makes BlokMiners stand apart from the crowd. We have taken their assistance in 3 projects and they have never failed us ever. I consider them our extended technology arm whenever we have constrained bandwidth.',
-            index: 1,
-        },
-        {
-            name: 'Second slide',
-            message:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime perferendis accusamus quidem facere fugiat numquam ducimus ab quis fugit, consequuntur minima, nobis consectetur? Odio facere rem aut hic? Eius!',
-            index: 2,
-        },
-        {
-            name: 'third slide',
-            message:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime perferendis accusamus quidem facere fugiat numquam ducimus ab quis fugit, consequuntur minima, nobis consectetur? Odio facere rem aut hic? Eius!',
-            index: 3,
-        },
-        {
-            name: 'fourth slide',
-            message:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime perferendis accusamus quidem facere fugiat numquam ducimus ab quis fugit, consequuntur minima, nobis consectetur? Odio facere rem aut hic? Eius!',
-            index: 4,
-        },
-        {
-            name: 'fifth slide',
-            message:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime perferendis accusamus quidem facere fugiat numquam ducimus ab quis fugit, consequuntur minima, nobis consectetur? Odio facere rem aut hic? Eius!',
-            index: 5,
-        },
-        {
-            name: 'six slide',
-            message:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime perferendis accusamus quidem facere fugiat numquam ducimus ab quis fugit, consequuntur minima, nobis consectetur? Odio facere rem aut hic? Eius!',
-            index: 6,
-        },
-    ])
-    const [swiperInstance, setSwiperInstance] = useState<Swiper | null>(null)
-    const [currentSlide, setCurrentSlide] = useState(0)
+  const [slideData] = useState([
+    {
+      name: 'First slide',
+      message:
+        'The dedication, integrity, and commitment coupled with the extensive knowledge in Web3.O makes BlokMiners stand apart from the crowd. We have taken their assistance in 3 projects and they have never failed us ever. I consider them our extended technology arm whenever we have constrained bandwidth.',
+      index: 1,
+    },
+    {
+      name: 'Second slide',
+      message:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime perferendis accusamus quidem facere fugiat numquam ducimus ab quis fugit, consequuntur minima, nobis consectetur? Odio facere rem aut hic? Eius!',
+      index: 2,
+    },
+    {
+      name: 'third slide',
+      message:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime perferendis accusamus quidem facere fugiat numquam ducimus ab quis fugit, consequuntur minima, nobis consectetur? Odio facere rem aut hic? Eius!',
+      index: 3,
+    },
+    {
+      name: 'fourth slide',
+      message:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime perferendis accusamus quidem facere fugiat numquam ducimus ab quis fugit, consequuntur minima, nobis consectetur? Odio facere rem aut hic? Eius!',
+      index: 4,
+    },
+    {
+      name: 'fifth slide',
+      message:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime perferendis accusamus quidem facere fugiat numquam ducimus ab quis fugit, consequuntur minima, nobis consectetur? Odio facere rem aut hic? Eius!',
+      index: 5,
+    },
+    {
+      name: 'six slide',
+      message:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime perferendis accusamus quidem facere fugiat numquam ducimus ab quis fugit, consequuntur minima, nobis consectetur? Odio facere rem aut hic? Eius!',
+      index: 6,
+    },
+  ])
+  const [swiperInstance, setSwiperInstance] = useState<Swiper | null>(null)
+  const [currentSlide, setCurrentSlide] = useState(0)
 
-    const handleSlideChange = (swiper: any) => {
-        setCurrentSlide(swiper.activeIndex)
-    }
+  const handleSlideChange = (swiper: any) => {
+    setCurrentSlide(swiper.activeIndex)
+  }
 
-    return (
-        <>
-            <div className='scale-x-[130%] transform'>
-                <Swiper
-                    onSwiper={setSwiperInstance}
-                    onSlideChange={handleSlideChange}
-                    spaceBetween={20}
-                    // Option 1: Ensure enough slides for centering (if applicable)
-                    slidesPerView={3} // Adjust based on your needs
-                    centeredSlides={true}
-                    // Option 2: Set a fixed container width (optional)
-                    // style={{ width: '800px' }} // Adjust based on your layout
-                    navigation={{
-                        prevEl: swiperInstance?.navigation?.prevEl,
-                        nextEl: swiperInstance?.navigation?.nextEl,
-                    }}
-                    className='w-full'
-                >
-                    {slideData.map((item, index) => (
-                        <SwiperSlide key={index} className={``}>
-                            <div
-                                className={`  ${index !== currentSlide && '!py-8'} mx-auto  h-[280px]`}
-                            >
-                                {/* <SwipeBox name={item.name} message={item.message} index={item.index} big={index === currentSlide} /> */}
-                                <div className='flex h-full flex-col justify-between rounded-xl  border-2 border-white border-opacity-20 px-8 !py-8  text-white overflow-hidden bg-white bg-opacity-5 transition-all '>
-                                    <div className='flex w-full justify-between items-center'>
-                                        <div className='flex items-center gap-2'>
-                                            <div className='h-12 w-12'>
-                                                <Image
-                                                    src={'/Corousel/zeeve.svg'}
-                                                    width={100}
-                                                    height={80}
-                                                    alt='zeeve'
-                                                />
-                                            </div>
-                                            <div>
-                                                <div>Zeeve</div>
-                                                <div className='text-gray-400'>CEO</div>
-                                            </div>
-                                        </div>
-                                        <div className='flex text-yellow-400 gap-1'>
-                                            <GoStarFill />
-                                            <GoStarFill />
-                                            <GoStarFill />
-                                            <GoStarFill />
-                                            <GoStarFill />
-                                        </div>
-                                    </div>
-                                    <div className={`${index !== currentSlide && 'text-xs'} text-sm`}>{item.message}</div>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
-            {/* <div className='mt-4 text-center'>
+  return (
+    <>
+      <div className='scale-x-[130%] transform'>
+        <Swiper
+          onSwiper={setSwiperInstance}
+          onSlideChange={handleSlideChange}
+          spaceBetween={20}
+          // Option 1: Ensure enough slides for centering (if applicable)
+          slidesPerView={3} // Adjust based on your needs
+          centeredSlides={true}
+          // Option 2: Set a fixed container width (optional)
+          // style={{ width: '800px' }} // Adjust based on your layout
+          navigation={{
+            prevEl: swiperInstance?.navigation?.prevEl,
+            nextEl: swiperInstance?.navigation?.nextEl,
+          }}
+          className='w-full'
+        >
+          {slideData.map((item, index) => (
+            <SwiperSlide key={index} className={``}>
+              <div
+                className={` ${index !== currentSlide && '!py-8'} mx-auto h-[300px]`}
+              >
+                {/* <SwipeBox name={item.name} message={item.message} index={item.index} big={index === currentSlide} /> */}
+                <div className=' flex h-full flex-col justify-between overflow-hidden rounded-xl border-2 border-white border-opacity-20 bg-white bg-opacity-5 !py-8 px-8 text-white transition-all'>
+                  <div className=' scale-y-[120%] flex w-full items-center justify-between'>
+                    <div className='flex items-center gap-2'>
+                      <div className='h-12 w-12'>
+                        <Image
+                          src={'/Corousel/zeeve.svg'}
+                          width={100}
+                          height={80}
+                          alt='zeeve'
+                        />
+                      </div>
+                      <div>
+                        <div>Zeeve</div>
+                        <div className='text-gray-400'>CEO</div>
+                      </div>
+                    </div>
+                    <div className='flex gap-1 text-yellow-400'>
+                      <GoStarFill />
+                      <GoStarFill />
+                      <GoStarFill />
+                      <GoStarFill />
+                      <GoStarFill />
+                    </div>
+                  </div>
+                  <div
+                    className={`${index !== currentSlide && 'text-xs'} scale-y-[120%] text-sm`}
+                  >
+                    {item.message}
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      {/* <div className='mt-4 text-center'>
                 <p className='text-white'>Current Slide: {currentSlide + 1}</p>
             </div> */}
-            {/* <button
+      {/* <button
                 onClick={() => swiperInstance?.slidePrev()}
                 className='bg-black text-white'
                 aria-label='Previous Slide'
@@ -133,16 +137,22 @@ const Corousel = () => {
                 next{' '}
             </button> */}
 
-            <div className='flex justify-center gap-4 mt-20'>
-                <button onClick={() => swiperInstance?.slidePrev()} className='p-3 text-white border-2 border-white border-opacity-20 bg-white bg-opacity-5 rounded-lg hover:bg-opacity-25'>
-                    <IoIosArrowBack className='w-6 h-6' />
-                </button>
-                <button onClick={() => swiperInstance?.slideNext()} className='p-3 text-white border-2 border-white border-opacity-20 bg-white bg-opacity-5 rounded-lg hover:bg-opacity-25'>
-                    <IoIosArrowForward className='w-6 h-6' />
-                </button>
-            </div>
-        </>
-    )
+      <div className='mt-20 flex justify-center gap-4'>
+        <button
+          onClick={() => swiperInstance?.slidePrev()}
+          className='rounded-lg border-2 border-white border-opacity-20 bg-white bg-opacity-5 p-3 text-white hover:bg-opacity-25'
+        >
+          <IoIosArrowBack className='h-6 w-6' />
+        </button>
+        <button
+          onClick={() => swiperInstance?.slideNext()}
+          className='rounded-lg border-2 border-white border-opacity-20 bg-white bg-opacity-5 p-3 text-white hover:bg-opacity-25'
+        >
+          <IoIosArrowForward className='h-6 w-6' />
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default Corousel
---
+++ lib/industries.tsx
@@ -0,0 +1,398 @@
+import Image from 'next/image'
+
+export const hoverContentData = {
+  'Finance & Banking': (
+    <div className='flex flex-col items-center rounded-lg bg-[#88caec] bg-opacity-10  p-10 text-center text-lg font-semibold text-theme-dark shadow-md'>
+      <h2 className='mb-6 text-2xl font-bold'>Finance & Banking</h2>
+      <p className='mb-10 text-theme-light'>
+        We empower the real estate sector to provide beyond property - the
+        commitment to enhancing clients' lifestyles
+      </p>
+      <h3 className='mb-10 text-2xl font-bold'>
+        Transforming Finance and Banking with BlokMiners’ Blockchain Solutions
+      </h3>
+      <div className='grid w-full grid-cols-2 gap-8'>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/image1.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Decentralized Finance <br /> (DeFi) Solutions
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image2.png'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Smart Contract <br /> Implementation
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image3.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Cross-Border <br /> Payments
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image4.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Regulatory Compliance <br /> and Reporting
+          </div>
+        </div>
+      </div>
+    </div>
+  ),
+  'Healthcare': (
+    <div className='flex flex-col items-center rounded-lg bg-[#88caec] bg-opacity-10  p-10 text-center text-lg font-semibold text-theme-dark shadow-md'>
+      <h2 className='mb-6 text-2xl font-bold'>Healthcare</h2>
+      <p className='mb-10 text-theme-light'>
+        We assist in achieving healthcare that is intelligent, interconnected,
+        and focused on the patient.
+      </p>
+      <h3 className='mb-10 text-2xl font-bold'>
+        Establishing a value-based network.
+      </h3>
+      <ul className='w-full list-outside list-disc pl-4 text-left'>
+        <li className='mb-4'>
+          Customers nowadays seek experiences, not solely products.
+        </li>
+        <li className='mb-4'>
+          Manufacturers need to construct connected, cognitive, and cooperative
+          networks that facilitate adaptable innovation on a grand scale.{' '}
+        </li>
+        <li className=''>
+          This necessitates an approach resembling the neural system, where the
+          manufacturing value chain imitates the human nervous
+          system—automatically detecting new data, grasping its significance,
+          and promptly reacting.
+        </li>
+      </ul>
+    </div>
+  ),
+  'Insurance': (
+    <div className='flex flex-col items-center rounded-lg bg-[#88caec] bg-opacity-10  p-10 text-center text-lg font-semibold text-theme-dark shadow-md'>
+      <h2 className='mb-6 text-2xl font-bold'>Insurance</h2>
+      <p className='mb-10 text-theme-light'>
+        We enable insurance providers to offer more than just security – the
+        assurance of an improved life for their clients.
+      </p>
+      <h3 className='mb-10 text-2xl font-bold'>
+        Innovative Approaches Deliver Customer Benefit
+      </h3>
+      <ul className='w-full list-outside list-disc pl-4 text-left'>
+        <li className='mb-4'>
+          Historically, insurers have supplied financial safeguarding against
+          life and property losses.{' '}
+        </li>
+        <li className='mb-4'>
+          Over the past ten years, their approaches have evolved to encompass
+          prevention and sustainability. Prevention alerts policyholders to
+          potential risks, while sustainability guarantees ongoing security.{' '}
+        </li>
+        <li className=''>
+          Encompassing this entire range—from protection to
+          sustainability—necessitates an adaptable business structure and a
+          digitally-focused approach.
+        </li>
+      </ul>
+    </div>
+  ),
+  'Education': (
+    <div className='flex flex-col items-center rounded-lg bg-[#88caec] bg-opacity-10  p-10 text-center text-lg font-semibold text-theme-dark shadow-md'>
+      <h2 className='mb-6 text-2xl font-bold'>Education</h2>
+      <p className='mb-10 text-theme-light'>
+        We empower educators to provide beyond knowledge – the promise of a
+        brighter future for their students
+      </p>
+      <h3 className='mb-10 text-2xl font-bold'>
+        Education and the Evolution of Learning Technology
+      </h3>
+      <ul className='w-full list-outside list-disc pl-4 text-left'>
+        <li className='mb-4'>
+          The 21st century heralded an education revolution, powered by the
+          internet’s perpetual learning resource.
+        </li>
+        <li className='mb-4'>
+          Modern disruptions underscore the necessity for learning technology
+          solutions, enriching blended learning experiences.
+        </li>
+        <li className='mb-4'>
+          Through digital learning solutions offering boundless educational
+          access via hybrid models, virtual campuses, extended reality, and the
+          emerging metaverse, students encounter personalized, immersive, and
+          self-paced learning.
+        </li>
+        <li className=''>
+          Moreover, industry-academia collaboration reimagines the educational
+          framework, cultivating a dynamic and future-ready workforce.
+        </li>
+      </ul>
+    </div>
+  ),
+  'Real Estate': (
+    <div className='flex flex-col items-center rounded-lg bg-[#88caec] bg-opacity-10  p-10 text-center text-lg font-semibold text-theme-dark shadow-md'>
+      <h2 className='mb-6 text-2xl font-bold'>Real Estate</h2>
+      <p className='mb-10 text-theme-light'>
+        We empower the real estate sector to provide beyond property – the
+        commitment to enhancing clients' lifestyles
+      </p>
+      <h3 className='mb-10 text-2xl font-bold'>
+        Building the Future of Real Estate on BlokMiners' Blockchain Platform
+      </h3>
+      <div className='grid w-full grid-cols-2 gap-8'>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/image1.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Decentralized Finance <br /> (DeFi) Solutions
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image2.png'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Smart Contract <br /> Implementation
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image3.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Cross-Border <br /> Payments
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image4.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Regulatory Compliance <br /> and Reporting
+          </div>
+        </div>
+      </div>
+    </div>
+  ),
+  'Travel & Logistics': (
+    <div className='flex flex-col items-center rounded-lg bg-[#88caec] bg-opacity-10  p-10 text-center text-lg font-semibold text-theme-dark shadow-md'>
+      <h2 className='mb-6 text-2xl font-bold'>Travel & Logistics</h2>
+      <p className='mb-10 text-theme-light'>
+        Embarking on Journeys: Crafting Seamless Travel and Logistics
+        Experiences
+      </p>
+      <h3 className='mb-10 text-2xl font-bold'>
+        Revolutionizing Travel and Logistics with BlokMiners' Advanced Solutions
+      </h3>
+      <div className='grid w-full grid-cols-2 gap-8'>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/image1.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Decentralized Finance <br /> (DeFi) Solutions
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image2.png'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Smart Contract <br /> Implementation
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image3.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Cross-Border <br /> Payments
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image4.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Regulatory Compliance <br /> and Reporting
+          </div>
+        </div>
+      </div>
+    </div>
+  ),
+  'Retail': (
+    <div className='flex flex-col items-center rounded-lg bg-[#88caec] bg-opacity-10  p-10 text-center text-lg font-semibold text-theme-dark shadow-md'>
+      <h2 className='mb-6 text-2xl font-bold'>Retail</h2>
+      <p className='mb-10 text-theme-light'>
+        We empower retailers to offer more than just products – the promise of
+        an enhanced lifestyle for their customers
+      </p>
+      <h3 className='mb-10 text-2xl font-bold'>
+        Creative Strategies Enhance Shopping Experience
+      </h3>
+      <ul className='w-full list-outside list-disc pl-4 text-left'>
+        <li className='mb-4'>
+          Traditionally, retailers have provided products for consumers. In
+          recent years, their methods have expanded to include personalization
+          and sustainability.
+        </li>
+        <li className='mb-4'>
+          Personalization tailors offerings to individual preferences, while
+          sustainability ensures lasting impact.
+        </li>
+        <li className=''>
+          Covering this spectrum—from customization to sustainability—requires a
+          flexible business model and a digitally-oriented strategy
+        </li>
+      </ul>
+    </div>
+  ),
+  'Manufacturing': (
+    <div className='flex flex-col items-center rounded-lg bg-[#88caec] bg-opacity-10  p-10 text-center text-lg font-semibold text-theme-dark shadow-md'>
+      <h2 className='mb-6 text-2xl font-bold'>Manufacturing</h2>
+      <p className='mb-10 text-theme-light'>
+        We transform enterprise value chains into ecosystems that sense, learn,
+        and adapt.
+      </p>
+      <h3 className='mb-10 text-2xl font-bold'>
+        Empowering Manufacturing with BlokMiners' Blockchain Solutions
+      </h3>
+      <div className='grid w-full grid-cols-2 gap-8'>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/image1.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Decentralized Finance <br /> (DeFi) Solutions
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image2.png'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Smart Contract <br /> Implementation
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image3.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Cross-Border <br /> Payments
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-[50px]'>
+            <Image
+              alt='image'
+              src={'/Industries/Image4.svg'}
+              width={100}
+              height={100}
+              className='w-full'
+            />
+          </div>
+          <div>
+            Regulatory Compliance <br /> and Reporting
+          </div>
+        </div>
+      </div>
+    </div>
+  ),
+}

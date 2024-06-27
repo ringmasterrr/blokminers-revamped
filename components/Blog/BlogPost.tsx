46aef464ac3bde10269b4e6e79e1c5466624f8d6
YashT1005 <yashthakur10052@gmail.com>
2024-06-27 15:59:00 +0530

    integration

--- components/Blog/BlogPost.tsx
+++ components/Blog/BlogPost.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { posts } from './Data'
 import { useEffect, useState } from 'react'
 import { getBlogById } from '@/services/blog'
+import { format, parseISO } from 'date-fns'
+import { CgSpinnerTwo } from "react-icons/cg";
 
 const blogPost = {
   title: 'Choosing the Right DApp Development Company – Factors to Consider',
@@ -36,90 +38,123 @@ const blogPost = {
 }
 
 export default function BlogPost({ postId }: { postId: string }) {
-  const [blog, setBlog] = useState({})
-  const [sections, setSections] = useState([])
+  const [blog, setBlog] = useState<any | null>(null)
+  const [sections, setSections] = useState<any | null>(null)
+  const [formattedDate, setFormattedDate] = useState<any | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
 
   const getBlog = async () => {
-    const res: any = await getBlogById('667c12092b9b90fcc8c15505')
-    setBlog(res.blog?.blog)
-    setSections(res.blog?.sections)
-  }
+    try {
+      const res: any = await getBlogById(postId)
+      console.log(res)
+      const fetchedBlog = res?.blog[0]
+      if (fetchedBlog) {
+        setBlog(fetchedBlog)
+        setSections(fetchedBlog.sections)
 
-  console.log('blog :', blog)
-  console.log('sections :', sections)
+        const dateString: string | undefined = fetchedBlog.createdAt
+        if (dateString) {
+          const parsedDate: Date = parseISO(dateString)
+          setFormattedDate(format(parsedDate, 'MMMM do, yyyy '))
+        }
+      }
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500);
+    } catch (error) {
+      console.error('Error fetching blog:', error)
+    }
+  }
 
   useEffect(() => {
     getBlog()
   }, [postId])
-
-  // const [data, setData] = useState(posts[post])
-  return (
-    <div className='bg-grid'>
-      <div className='flex w-full items-center justify-between gap-5 pb-10 pt-28'>
-        <div className='h-[100%] w-[50%] p-8'>
-          <h1 className='mb-6 text-[52px] font-extrabold leading-tight'>
-            {blogPost.title}
-          </h1>
-          {blogPost.paragraph.map((paragraph, index) => (
-            <p
-              key={index}
-              className='mb-6 text-[20px] font-medium text-gray-700'
-            >
-              {paragraph}
-            </p>
-          ))}
-          <div className='flex items-center space-x-4'>
-            <div className='flex items-center rounded-full border-2 border-theme-dark/50 px-2'>
-              <Image
-                src={blogPost.author.avatar}
-                alt='Coding on a laptop'
-                height={1000}
-                width={1000}
-                className='h-5 w-5 rounded-full object-cover'
-              />
-              <span className='ml-2 text-xl text-theme-dark'>
-                {blogPost.author.name}
-              </span>
+  if (loading) {
+    return (
+      <div className='flex h-screen text-blue-700 items-center justify-center'>
+        <CgSpinnerTwo className='animate-spin text-8xl' />
+      </div>
+    )
+  } else
+    return (
+      <>
+        {blog && (
+          <div className='bg-grid'>
+            <div className='flex w-full items-center justify-between gap-5 pb-10 pt-28'>
+              <div className='h-[100%] w-[50%] p-8'>
+                <h1 className='mb-6 text-[52px] font-extrabold leading-tight'>
+                  {blog?.title}
+                </h1>
+                <p className='mb-6 text-[20px] font-medium text-gray-700'>
+                  {blog?.summary}
+                </p>
+                {/* {blogPost.paragraph.map((paragraph, index) => (
+                <p
+                  key={index}
+                  className='mb-6 text-[20px] font-medium text-gray-700'
+                >
+                  
+                </p>
+              ))} */}
+                <div className='flex items-center space-x-4'>
+                  <div className='flex items-center rounded-full border-2 border-theme-dark/50 px-2'>
+                    <Image
+                      src={blogPost.author.avatar}
+                      alt='Coding on a laptop'
+                      height={1000}
+                      width={1000}
+                      className='h-5 w-5 rounded-full object-cover'
+                    />
+                    <span className='ml-2 text-xl text-theme-dark'>
+                      {blog.authorName}
+                    </span>
+                  </div>
+                  <div className='flex items-center rounded-full border-2 border-theme-dark/50 px-2'>
+                    <span className='text-xl text-theme-dark'>
+                      {formattedDate}
+                    </span>
+                  </div>
+                  {/* <div className='flex items-center rounded-full border-2 border-theme-dark/50 px-2'>
+                  <span className='text-xl text-theme-dark'>
+                    {blogPost.readTime}
+                  </span>
+                </div> */}
+                </div>
+              </div>
+              <div className='h-[100%] w-[50%]'>
+                <Image
+                  src={blogPost.image}
+                  alt='Coding on a laptop'
+                  height={1000}
+                  width={1000}
+                  className='object-cover'
+                />
+              </div>
+              <div className='bg-nav bg-blur absolute -top-[60rem] left-[20rem] bg-[#D8F6FF]'></div>
             </div>
-            <div className='flex items-center rounded-full border-2 border-theme-dark/50 px-2'>
-              <span className='text-xl text-theme-dark'>{blogPost.date}</span>
+            <div className='p-8'>
+              <p className='text-[20px] font-medium text-gray-700'>
+                {blogPost.HighlightedPara}
+              </p>
             </div>
-            <div className='flex items-center rounded-full border-2 border-theme-dark/50 px-2'>
-              <span className='text-xl text-theme-dark'>
-                {blogPost.readTime}
-              </span>
+            <div className='my-24 p-8'>
+              <div className='w-1/3'>
+                <div className='mb-10 text-[20px] font-semibold text-theme-dark'>
+                  Contents
+                </div>
+                {sections &&
+                  sections.map((item: any, index: any) => (
+                    <p
+                      key={index}
+                      className='mb-6 text-[20px] font-bold text-theme-light hover:text-theme-dark'
+                    >
+                      {item.title}
+                    </p>
+                  ))}
+              </div>
             </div>
           </div>
-        </div>
-        <div className='h-[100%] w-[50%]'>
-          <Image
-            src={blogPost.image}
-            alt='Coding on a laptop'
-            height={1000}
-            width={1000}
-            className='object-cover'
-          />
-        </div>
-        <div className='bg-nav bg-blur absolute -top-[60rem] left-[20rem] bg-[#D8F6FF]'></div>
-      </div>
-      <div className='p-8'>
-        <p className='text-[20px] font-medium text-gray-700'>
-          {blogPost.HighlightedPara}
-        </p>
-      </div>
-      <div className='my-24 p-8'>
-        <div className='w-1/3'>
-        <div className='mb-10 text-[20px] font-semibold text-theme-dark'>Contents</div>
-          {blogPost.content.map((content, index) => (
-            <p
-              key={index}
-              className='mb-6 text-[20px] font-bold text-theme-light hover:text-theme-dark '
-            >
-              {content.title}
-            </p>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
+        )}
+      </>
+    )
 }
--- components/Blog/PostList.tsx
+++ components/Blog/PostList.tsx
@@ -1,88 +1,89 @@
 // components/PostList.tsx
-"use client"
-import Link from 'next/link';
-import Image from 'next/image';
-import { useEffect } from 'react';
+'use client'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useEffect } from 'react'
+import { Url } from 'url'
 
 interface Post {
-  id: number;
-  title: string;
-  body: string;
-  image: string;
-  date: string;
-  time: string;
+  _id: number
+  title: string
+  summary: string
+  image: string
+  date: string
+  createdAt: string
+  authorName: string
 }
 
 interface PostListProps {
-  posts: Post[];
+  posts: Post[]
 }
 
 const truncateText = (text: string, wordLimit: number) => {
-  const words = text.split(' ');
+  const words = text.split(' ')
   if (words.length <= wordLimit) {
-    return text;
+    return text
   }
-  return words.slice(0, wordLimit).join(' ') + '...';
-};
+  return words.slice(0, wordLimit).join(' ') + '...'
+}
 
 const PostList = ({ posts }: PostListProps) => {
-
-
-  
-
   return (
     <div className='grid grid-cols-3 gap-6'>
-      {posts.map((post) => (
-        <Link href={`/blogpost/${post.id}`}>
-          <div className='mb-4 shadow-custom rounded-xl border border-[#00283C29] bg-white cursor-pointer'>
-            <Image
-              src={post.image}
-              alt={post.title}
-              height={1500}
-              width={1500}
-              className='mb-4 h-48 w-full rounded-xl object-cover'
-            />
-            <div className='p-4 flex flex-col gap-2'>
-              <div className='flex gap-2'>
-                <p className='mb-2 flex items-center gap-1 text-[#A3A3A3] text-sm font-medium'>
+      {posts &&
+        posts.map((post) => (
+          <Link href={`/blogpost/${post._id}`}>
+            <div className='shadow-custom mb-4 cursor-pointer rounded-xl border border-[#00283C29] bg-white'>
+              <Image
+                src={"https://picsum.photos/200"}
+                alt={post.title}
+                height={1500}
+                width={1500}
+                className='mb-4 h-48 w-full rounded-xl object-cover'
+              />
+              <div className='flex flex-col gap-2 p-4'>
+                <div className='flex gap-2'>
+                  <p className='mb-2 flex items-center gap-1 text-sm font-medium text-[#A3A3A3]'>
+                    <Image
+                      src={'/date.svg'}
+                      alt={'icon'}
+                      height={20}
+                      width={20}
+                      className='text-[#A3A3A3]'
+                    />
+                    {post.date}
+                  </p>
+                  <span className='text-[#A3A3A3]'>|</span>
+                  <p className='mb-2 flex items-center gap-1 text-sm font-medium text-[#A3A3A3]'>
+                    <Image
+                      src={'/time.svg'}
+                      alt={'icon'}
+                      height={20}
+                      width={20}
+                      className='text-[#A3A3A3]'
+                    />
+                    {post.createdAt}
+                  </p>
+                </div>
+                <h2 className='mb-2 text-2xl font-bold'>{post.title}</h2>
+                <p className='text-[#A3A3A3]'>{truncateText(post.summary, 16)}</p>
+                <button className='group my-4 flex items-center gap-2 text-sm font-semibold'>
+                  Read now
                   <Image
-                    src={'/date.svg'}
+                    src={'/arrow.svg'}
                     alt={'icon'}
                     height={20}
                     width={20}
-                    className='text-[#A3A3A3]'
+                    className='transform transition-transform duration-300 group-hover:-rotate-45'
                   />
-                  {post.date}
-                </p><span className='text-[#A3A3A3]'>|</span>
-                <p className='mb-2 flex items-center gap-1 text-[#A3A3A3] text-sm font-medium'>
-                  <Image
-                    src={'/time.svg'}
-                    alt={'icon'}
-                    height={20}
-                    width={20}
-                    className='text-[#A3A3A3]'
-                  />
-                  {post.time}
-                </p>
+                </button>
               </div>
-              <h2 className='mb-2 text-2xl font-bold'>{post.title}</h2>
-              <p className='text-[#A3A3A3]'>{truncateText(post.body, 16)}</p>
-              <button className='flex items-center gap-2 my-4 group text-sm font-semibold'>
-                Read now
-                <Image
-                  src={'/arrow.svg'}
-                  alt={'icon'}
-                  height={20}
-                  width={20}
-                  className='transform transition-transform duration-300 group-hover:-rotate-45'
-                />
-              </button>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
     </div>
-  );
-};
+    // <div>Posts</div>
+  )
+}
 
-export default PostList;
+export default PostList

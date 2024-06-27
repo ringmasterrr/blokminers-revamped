f5f680e528b645f254f70fb1dc596f01615464b0
ringmasterrr <thevagish@gmail.com>
2024-06-27 19:42:02 +0530

    blogs

--- components/Blog/BlogPost.tsx
+++ components/Blog/BlogPost.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Image from 'next/image'
-import { posts } from './Data'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { getBlogById } from '@/services/blog'
 import { format, parseISO } from 'date-fns'
 import { CgSpinnerTwo } from 'react-icons/cg'
@@ -11,7 +10,9 @@ export default function BlogPost({ postId }: { postId: string }) {
   const [sections, setSections] = useState<any | null>(null)
   const [formattedDate, setFormattedDate] = useState<any | null>(null)
   const [loading, setLoading] = useState<boolean>(true)
-  console.log(sections)
+  const sectionRefs = useRef<HTMLDivElement[]>([])
+  const [activeIndex, setActiveIndex] = useState<number | null>(null)
+  const navbarHeight = 80
 
   const getBlog = async () => {
     try {
@@ -39,6 +40,45 @@ export default function BlogPost({ postId }: { postId: string }) {
   useEffect(() => {
     getBlog()
   }, [postId])
+
+  const scrollToSection = (index: number) => {
+    if (sectionRefs.current[index]) {
+      const element = sectionRefs.current[index]
+      const elementPosition =
+        element.getBoundingClientRect().top + window.scrollY
+      const offsetPosition = elementPosition - navbarHeight
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth',
+      })
+    }
+  }
+
+  // Function to handle scroll and update active section
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY + navbarHeight + 20 // Adjusted for better accuracy
+    const newActiveIndex = sectionRefs.current.findIndex((ref) => {
+      if (ref) {
+        const top = ref.getBoundingClientRect().top + window.scrollY
+        const bottom = top + ref.clientHeight
+        return scrollPosition >= top && scrollPosition <= bottom
+      }
+      return false
+    })
+
+    if (newActiveIndex !== activeIndex) {
+      setActiveIndex(newActiveIndex)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   if (loading) {
     return (
       <div className='flex h-screen items-center justify-center text-blue-700'>
@@ -59,37 +99,32 @@ export default function BlogPost({ postId }: { postId: string }) {
                   {blog?.summary}
                 </p>
                 <div className='flex items-center space-x-4'>
-                  <div className='flex items-center rounded-full border-2 border-theme-dark/50 px-2'>
+                  <div className='flex h-8 items-center rounded-full border-2 border-theme-dark/50 px-4'>
                     <Image
                       src={`/avatar.png`}
-                      alt='Coding on a laptop'
+                      alt='Author avatar'
                       height={1000}
                       width={1000}
                       className='h-8 w-8 rounded-full object-cover'
                     />
-                    <span className='ml-2 text-xl text-theme-dark'>
+                    <span className='text-xl text-theme-dark'>
                       {blog.authorName}
                     </span>
                   </div>
-                  <div className='flex items-center rounded-full border-2 border-theme-dark/50 px-2'>
+                  <div className='flex h-8 items-center rounded-full border-2 border-theme-dark/50 px-4'>
                     <span className='text-xl text-theme-dark'>
                       {formattedDate}
                     </span>
                   </div>
-                  {/* <div className='flex items-center rounded-full border-2 border-theme-dark/50 px-2'>
-                  <span className='text-xl text-theme-dark'>
-                    {blogPost.readTime}
-                  </span>
-                </div> */}
                 </div>
               </div>
-              <div className='h-[100%] w-[50%]'>
+              <div className='flex h-[100%] w-[40%] items-center justify-center'>
                 <Image
                   src={blog.image}
-                  alt='Coding on a laptop'
+                  alt='Blog image'
                   height={1000}
                   width={1000}
-                  className='object-cover'
+                  className='max-w-[30rem] object-cover'
                 />
               </div>
               <div className='bg-nav bg-blur absolute -top-[60rem] left-[20rem] bg-[#D8F6FF]'></div>
@@ -104,7 +139,10 @@ export default function BlogPost({ postId }: { postId: string }) {
                   sections.map((item: any, index: any) => (
                     <p
                       key={index}
-                      className='mb-6 text-[20px] font-bold text-theme-dark'
+                      className={`mb-6 cursor-pointer rounded-md p-2 text-[20px] font-bold ${
+                        activeIndex === index ? 'bg-[#0096E114]' : ''
+                      }`}
+                      onClick={() => scrollToSection(index)}
                     >
                       {item.title}
                     </p>
@@ -113,7 +151,13 @@ export default function BlogPost({ postId }: { postId: string }) {
               <div className='w-[744px]'>
                 {sections &&
                   sections.map((item: any, index: any) => (
-                    <div key={index} className='mb-16 mt-16 first:mt-0'>
+                    <div
+                      key={index}
+                      className='mb-16 mt-16 first:mt-0'
+                      ref={(el) => {
+                        sectionRefs.current[index] = el as HTMLDivElement
+                      }}
+                    >
                       <p className='mb-6 w-[85%] text-4xl font-bold leading-tight text-theme-dark'>
                         {item.title}
                       </p>
@@ -121,7 +165,7 @@ export default function BlogPost({ postId }: { postId: string }) {
                         <div className='w-full'>
                           <Image
                             src={item.image}
-                            alt='Coding on a laptop'
+                            alt='Section image'
                             height={1000}
                             width={1000}
                             className='w-full object-cover'

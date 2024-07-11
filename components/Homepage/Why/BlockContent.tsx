83d1ace3eadde18ef7757486bfc407063f5bce2a
ringmasterrr <thevagish@gmail.com>
2024-07-11 19:07:33 +0530

    fix

--- components/Homepage/Why/BlockContent.tsx
+++ components/Homepage/Why/BlockContent.tsx
@@ -13,24 +13,24 @@ export const items = [
     title: 'Tech Advantage',
     description:
       "We leverage expertise in blockchain and AI/ML for clients' competitive edge in today's dynamic market.",
-    image: '/Why/tech.svg',
+    image: '/why/tech.svg',
   },
   {
     title: 'Innovative Deliveries',
     description:
       'BlokMiners consistently delivers innovative solutions with tangible, measurable results.',
-    image: '/Why/deliveries.svg',
+    image: '/why/deliveries.svg',
   },
   {
     title: 'Customized Offerings',
     description:
       "We offer tailored solutions, understanding each business's unique objectives.",
-    image: '/Why/offerings.svg',
+    image: '/why/offerings.svg',
   },
   {
     title: 'Security Priority',
     description:
       'We provide secure blockchain implementations and cybersecurity measures to safeguard digital assets.',
-    image: '/Why/security.svg',
+    image: '/why/security.svg',
   },
 ]

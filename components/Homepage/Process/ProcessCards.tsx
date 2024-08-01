ed99255813d6fee50e7ca4df4efaa61dd1974a91
YashT1005 <yashthakur10052@gmail.com>
2024-08-01 16:21:20 +0530

    processes

--- components/Homepage/Process/ProcessCards.tsx
+++ components/Homepage/Process/ProcessCards.tsx
@@ -15,37 +15,37 @@ const content = [
     title: 'Architect',
     description:
       'This is the skeletal framework of the project and at this point, we decide the Blockchain protocol based on type, energy efficiency, bridging strength and cost. We’ll also develop frameworks and determine smart contracts, business logic and storage.',
-    image: '/OurProcess/architect.svg',
+    image: '/OurProcess/architect1.svg',
   },
   {
     title: 'Design',
     description:
       'We lay out a detailed conceptual workflow by collaborating closely with you to see how your idea becomes a product. We’ll determine the relevance of Blockchain to your project, the best Blockchain model to deploy, gain clarity on the specifics of your idea and work with it.',
-    image: '/OurProcess/design.svg',
+    image: '/OurProcess/design1.svg',
   },
   {
     title: 'Build',
     description:
       'This is the skeletal framework of the project and at this point, we decide the Blockchain protocol based on type, energy efficiency, bridging strength and cost. We’ll also develop frameworks and determine smart contracts, business logic and storage.',
-    image: '/OurProcess/build.svg',
+    image: '/OurProcess/build1.svg',
   },
   {
     title: 'Test',
     description:
       'We lay out a detailed conceptual workflow by collaborating closely with you to see how your idea becomes a product. We’ll determine the relevance of Blockchain to your project, the best Blockchain model to deploy, gain clarity on the specifics of your idea and work with it.',
-    image: '/OurProcess/test.svg',
+    image: '/OurProcess/test1.svg',
   },
   {
     title: 'Launch',
     description:
       'We’re present through every stage in the journey even after we’ve delivered your decentralised application. Safely deploy your products at this stage, you’ll typically gain traction all things being equal and also get more feedback to further solidify the app.',
-    image: '/OurProcess/launch.svg',
+    image: '/OurProcess/launch1.png',
   },
   {
     title: 'Upgrade',
     description:
       'There’s no perfect idea or software. You can only pay excellent attention to the most unnecessary details like we do. Regular updates and reviews keep your software alive as the world evolves. You can always count on our expert Blockchain tech team to readily deliver these solid upgrades.',
-    image: '/OurProcess/upgrade.svg',
+    image: '/OurProcess/upgrade1.svg',
   },
 ]
 
@@ -90,12 +90,13 @@ export const ProcessCards = () => {
                 <div className='text-3xl font-bold'>{item.title}</div>
                 <div>{item.description}</div>
               </div>
-              <div className='glassy-bg py-18 flex h-[24rem] w-[20rem] items-center justify-center rounded-2xl p-8 2md:w-[36rem]'>
+              <div className='glassy-bg flex h-[24rem] w-[20rem] items-center justify-center rounded-2xl p-8 py-10 2md:w-[36rem]'>
                 <Image
                   src={item.image}
                   alt={item.title}
                   width={300}
                   height={300}
+                  className='h-full 2md:w-full'
                 />
               </div>
             </motion.div>
@@ -107,12 +108,13 @@ export const ProcessCards = () => {
               variants={animationVariants.fadeInRight}
               className='flex flex-col items-center justify-center 2md:flex-row'
             >
-              <div className='glassy-bg py-18 flex h-[24rem] w-[20rem] translate-x-5 translate-y-5 items-center justify-center rounded-2xl p-8 2md:w-[36rem] 2md:translate-x-20 2md:translate-y-0'>
+              <div className='glassy-bg flex h-[24rem] w-[20rem] translate-x-5 translate-y-5 items-center justify-center rounded-2xl p-8 !py-10 2md:w-[36rem] 2md:translate-x-20 2md:translate-y-0'>
                 <Image
                   src={item.image}
                   alt={item.title}
                   width={300}
                   height={300}
+                  className='h-full 2md:w-full'
                 />
               </div>
               <div className='glassy-bg flex h-[21rem] w-[20rem] flex-col items-start justify-center space-y-4 rounded-2xl p-8 backdrop-blur-lg 2md:w-[36rem]'>
